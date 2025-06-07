37495dea1ce9b0f08ddd7b3c066099ac3099712b
GeenaEvangelin04 <evangelingeena@gmail.com>
2025-06-07 05:30:08 +0530

    Made additional changes to app.py

---
+++ gui/app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Expense Tracker GUI")
+
+# Add your widgets here
+label = tk.Label(root, text="Welcome to Expense Tracker!")
+label.pack()
+
+root.mainloop()
